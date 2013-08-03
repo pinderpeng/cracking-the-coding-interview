b156c7cf7d4a99ade61cf5cb6fbfce678490b031
Pinder Peng <pinderpeng@gmail.com>
2013-08-03 18:25:03 +0800

    add code

--- src/Recursion/Parentheses.java
+++ src/Recursion/Parentheses.java
@@ -1,18 +1,19 @@
 package Recursion;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Parentheses {
 
-	public static ArrayList<String> generateParenttheses(int n){
+	public static HashSet<String> generateParenttheses(int n){
 		if(n == 1){
-			ArrayList<String> ans = new ArrayList<String>();
+			HashSet<String> ans = new HashSet<String>();
 			ans.add("()");
 			return ans;
 		}
 		
-		ArrayList<String> sub = generateParenttheses(n-1);
-		ArrayList<String> ans = new ArrayList<String>();
+		HashSet<String> sub = generateParenttheses(n-1);
+		HashSet<String> ans = new HashSet<String>();
 		for(String s : sub){
 			ans.add("(" + s + ")");
 			ans.add("()" + s);
@@ -21,9 +22,19 @@ public class Parentheses {
 		return ans;
 	}
 	
-	//(()(
-	public static ArrayList<String> generateParenttheses2(int n, int lcnt){
+	public static void generateParenttheses2(int n, int lcnt, String str, int N, HashSet<String> ans){
+		if(n >= N){
+			ans.add(str);
+			return;
+		}
 		
+		if(n+1-lcnt <= N/2 && lcnt >= n+1-lcnt){
+			generateParenttheses2(n+1, lcnt, str + ")", N, ans);
+		}
+		
+		if(lcnt+1 <= N/2){
+			generateParenttheses2(n+1, lcnt+1, str + "(", N, ans);
+		}
 	}
 	
 	/**
@@ -31,8 +42,16 @@ public class Parentheses {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> ans = generateParenttheses(3);
+		HashSet<String> ans = generateParenttheses(3);
+		System.out.println("size: " + ans.size());
 		for(String s : ans){
+			System.out.println(s);
+		}
+		
+		HashSet<String> ans2 = generateParenttheses(3);
+		System.out.println("size: " + ans2.size());
+		generateParenttheses2(1, 0, "", 3, ans2);
+		for(String s : ans2){
 			System.out.println(s);
 		}
 	}
